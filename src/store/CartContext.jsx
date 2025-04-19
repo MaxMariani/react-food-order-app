64ef011cdcd4c4bb09877e6ae16b8e31b4cf8cba
MaxMariani <wmaxmariani@gmail.com>
2025-04-19 14:09:00 +0200

    Added Modal Component

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,10 +1,11 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer} from "react";
+
 
 const CartContext = createContext({
-  items: [],
-  addItem: (item) => {},
-  removeItem: (id) => {},
-});
+    items: [],
+    addItem: (item) => {},
+    removeItem: (id) => {},
+  });
 
 function cartReducer(state, action) {
   if (action.type == "ADD_ITEM") {
@@ -24,21 +25,54 @@ function cartReducer(state, action) {
     } else {
       updatedItems.push({ ...action.item, quantity: 1 });
     }
-    
+
     return { ...state, items: updatedItems };
   }
 
   if (action.type == "REMOVE_ITEM") {
-    //TODO ...remove an item from the state
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedItems = [...state.items];
+    if (existingCartItem.quantity === 1) {
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updatedItems };
   }
 
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
 
-  return <CartContext>{children}</CartContext>;
+  function addItem(item) {
+    dispatchCartAction({type: 'ADD_ITEM', item})
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({type: 'REMOVE_ITEM', id})
+  }
+
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem
+  };
+
+  console.log(cartContext);
+
+  return <CartContext value={cartContext}>{children}</CartContext>;
 }
 
 export default CartContext;
