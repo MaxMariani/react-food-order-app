f86bf964656d4843dd072aeaedc6c8a8f3610344
MaxMariani <wmaxmariani@gmail.com>
2025-04-19 14:54:46 +0200

    Added UserProgressContext

---
+++ src/store/UserProgressContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+
+const UserProgressContext = createContext({
+  progress: "", // 'cart', 'checkout'
+  showCart: () => {},
+  hideCart: () => {},
+  showCheckout: () => {},
+  hideCheckout: () => {},
+});
+
+export function UserProgressContextProvider({ children }) {
+  const [userProgress, setUserProgress] = useState("");
+
+  function showCart() {
+    setUserProgress("cart");
+  }
+
+  function showCheckout() {
+    setUserProgress("checkout");
+  }
+
+  function hideCart() {
+    setUserProgress("");
+  }
+
+  function hideCheckout() {
+    setUserProgress("");
+  }
+
+  const userProgressCtx = {
+    progress: userProgress,
+    showCart,
+    hideCart,
+    showCheckout,
+    hideCheckout,
+  };
+
+  return (
+    <UserProgressContext value={userProgressCtx}>
+      {children}
+    </UserProgressContext>
+  );
+}
+
+export default UserProgressContext;
